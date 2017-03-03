c30dc4ce5ced216e9f076093c29d0d7910c7c750
Hlebetz <naskru@mail.ru>
2017-03-03 11:26:40 +0300

    Add right stick absolute omni movement

--- src/turtle_teleop_joy.cpp
+++ src/turtle_teleop_joy.cpp
@@ -13,18 +13,17 @@ private:
     ros::NodeHandle nh_;
 
     int linear_, angular_;
-    double scale_lin_, scale_ang_;
+    double scale_left_, scale_right_, scale_cross_;
     ros::Publisher vel_pub_;
     ros::Subscriber joy_sub_;
-
 };
 
 
-TeleopTurtle::TeleopTurtle(): linear_(1), angular_(2) {
-    nh_.param("axis_linear", linear_, linear_);
-    nh_.param("axis_angular", angular_, angular_);
-    nh_.param("scale_angular", scale_ang_, scale_ang_);
-    nh_.param("scale_linear", scale_lin_, scale_lin_);
+TeleopTurtle::TeleopTurtle(): scale_left_(1), scale_right_(1), scale_cross_(1) {
+    // nh_.param("axis_linear", linear_, linear_);
+    // nh_.param("axis_angular", angular_, angular_);
+    nh_.param("scale_left", scale_left_, scale_left_);
+    nh_.param("scale_right", scale_right_, scale_right_);
 
     vel_pub_ = nh_.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
 
@@ -32,9 +31,25 @@ TeleopTurtle::TeleopTurtle(): linear_(1), angular_(2) {
 }
 
 void TeleopTurtle::joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
+    float js_left_x = joy->axes[0];
+    float js_left_y = joy->axes[1];
+    float js_right_x = joy->axes[2];
+    float js_right_y = joy->axes[3];
+    float js_cross_x = joy->axes[4];
+    float js_cross_y = joy->axes[5];
+    int js_A = joy->buttons[0];
+    int js_B = joy->buttons[1];
+    int js_C = joy->buttons[2];
+    int js_D = joy->buttons[3];
+    int js_L1 = joy->buttons[4];
+    int js_R1 = joy->buttons[5];
+    int js_L2 = joy->buttons[6];
+    int js_R2 = joy->buttons[7];
+
     geometry_msgs::Twist twist;
-    twist.angular.z = scale_ang_*joy->axes[angular_];
-    twist.linear.x = scale_lin_*joy->axes[linear_];
+    twist.linear.x = -scale_right_ * js_right_x;
+    twist.linear.y = scale_right_ * js_right_y;
+
     vel_pub_.publish(twist);
 }
 
